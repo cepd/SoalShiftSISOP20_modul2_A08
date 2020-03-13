eab5b162d5233f45ecc31f7dee199037cfd29744
chintyapremadewi <62136051+chintyapremadewi@users.noreply.github.com>
2020-03-13 16:41:51 +0700

    Create Soal1.c

---
+++ Soal1.c
@@ -0,0 +1,82 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+time_t currenttime = time(NULL);
+struct tm tm = *localtime(&currenttime);
+
+if(argc!=5){
+	printf("error");
+	return 0;
+}
+
+
+int seconds = -1;
+if(*argv[1]>'9' || *argv[1]<'0') printf("error");
+else if(*argv [1] != '*') seconds = atoi (argv[1]);
+
+int minutes = -1;
+if(*argv[2]>'9' || *argv[2]<'0') printf("error");
+else if(*argv [2] != '*') minutes = atoi (argv[2]);
+
+int hours = -1;
+if(*argv[3]>'9' || *argv[3]<'0') printf("error");
+else if(*argv [3] != '*') seconds = atoi (argv[3]);
+
+if(seconds<0 ||seconds>59) printf("error");
+if(minutes<0 ||minutes>59) printf("error");
+if(hours<0 ||hours>23) printf("error");
+
+  while (1) {
+  //program
+currenttime = time(NULL);
+tm = *localtime(&currenttime);
+if((tm.tm_sec == seconds || seconds == -1) && (tm.tm_min == minutes || minutes == -1) && (tm.tm_hour == hours || hours == -1)){
+	if (fork() == 0) {
+		char *argexec[] = {"bash", argv[4], NULL};
+		execv("/bin/bash", argexec);
+	}
+
+    sleep(1);
+  }
+}
+}
